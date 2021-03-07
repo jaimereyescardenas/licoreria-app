12c42f34e04b1481a92a107f18bf755471398730
Jaime Reyes Cárdenas <jaimefabianreyescardenas@gmail.com>
2021-03-07 00:47:29 -0400

    Initial commit, added dependencies and basic structure

---
+++ src/test/java/cl/challenge/licoreria/LicoreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.challenge.licoreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicoreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
