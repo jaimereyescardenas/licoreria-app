c3628d96d66cc3b69258f8358d69e4a97891d6e3
Jaime Reyes Cárdenas <jaimefabianreyescardenas@gmail.com>
2021-03-07 11:08:53 -0400

    Edited entities, added a bidirectional relationship

--- src/main/java/cl/challenge/licoreria/entity/Category.java
+++ src/main/java/cl/challenge/licoreria/entity/Category.java
@@ -1,26 +1,38 @@
 package cl.challenge.licoreria.entity;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Entity
 @Table(name = "category")
 @Getter @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties(
+  ignoreUnknown = true, 
+  value = {
+	"hibernateLazyInitializer",
+	"handler"})
 public class Category {
 	
 	@Id
 	@Column(name = "id")
 	private Integer id;
 	
-	@Column(name = "name", nullable = false, length = 50)
+	@Column(name = "name", length = 50, nullable = false)
 	private String name;
+	
+	@OneToMany(mappedBy = "category")
+	private Set<Product> products;
 
 }
--- src/main/java/cl/challenge/licoreria/entity/Product.java
+++ src/main/java/cl/challenge/licoreria/entity/Product.java
@@ -7,6 +7,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,22 +17,28 @@ import lombok.Setter;
 @Table(name = "product")
 @Getter @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties(
+  ignoreUnknown = true, 
+  value = {
+	"hibernateLazyInitializer",
+	"handler",
+	"category"})
 public class Product {
 
 	@Id
 	@Column(name = "id")
 	private Integer id;
 	
-	@Column(name = "name", length = 50)
+	@Column(name = "name", length = 50, nullable = false)
 	private String name;
 	
-	@Column(name = "url_image", length = 150)
+	@Column(name = "url_image", length = 150, nullable = true)
 	private String urlImage;
 	
-	@Column(name = "price")
+	@Column(name = "price", nullable = false)
 	private Double price;
 	
-	@Column(name = "discount")
+	@Column(name = "discount", nullable = false)
 	private Integer discount;
 	
 	@ManyToOne
