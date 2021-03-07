80fc209fcd845cc6f0907d432c474dcb5dac8432
Jaime Reyes Cárdenas <jaimefabianreyescardenas@gmail.com>
2021-03-07 14:59:40 -0400

    Added discount prices for products and categories select display

--- src/main/java/cl/challenge/licoreria/controller/HomeController.java
+++ src/main/java/cl/challenge/licoreria/controller/HomeController.java
@@ -1,13 +1,13 @@
 package cl.challenge.licoreria.controller;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import cl.challenge.licoreria.entity.Category;
 import cl.challenge.licoreria.entity.Product;
 import cl.challenge.licoreria.service.ICategoryService;
 import cl.challenge.licoreria.service.IProductService;
@@ -25,10 +25,13 @@ public class HomeController {
 	@GetMapping("/")
 	public ModelAndView homePage() {
 		ModelAndView mav = new ModelAndView("home");
+		
+		List<Category> categories = categoryService.findAll();
+		
 		List<Product> products = productService.findAll();
-		
 		List<Product> formattedProducts = ProductUtils.formatProducts(products);
-		
+
+		mav.addObject("categories", categories);
 		mav.addObject("products", formattedProducts);
 		return mav;
 	}
